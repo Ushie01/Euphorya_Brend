0451f0f2b9b849660255ea35d1941f5e7560cc40
Eugene44-hub <eugeneishado44@gmail.com>
2022-04-30 02:18:30 +0100

    optimised code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {  Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Slider from './components/Slider'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,8 +7,11 @@ import New from './components/New'
 import Home from './components/Home'
 import About from './components/About'
 import Footer from './components/Footer'
-import ErrorBoundary from './components/ErrorBoundary'
 
+// assets
+// components
+// pages
+// utilities 
 
 const App = () => {
 
@@ -19,27 +22,26 @@ const App = () => {
     <Route path='/'
                 element={
                   <>
-                   
                     <Slider className='slider' />
                     <Home />
-                    <Footer />
                   </>
                 }
               />
               <Route path='/New'
                 element={
-                  <>
+               
                     <New />
-                  </>
+              
                 } />
               <Route path='/About'
                 element={
-                  <>
+           
                     <About />
-                  </>
+            
                 }
               />          
-      </Routes >    
+      </Routes >  
+      <Footer />  
     </>
        
           
